c9634dd6e47c287776a70e379ca8f61a5716cc6f
Nalyd59 <dy.crepin@gmail.com>
2023-06-12 15:04:53 +0200

    architecture

---
+++ php/includes/ul.inc.php
@@ -0,0 +1,9 @@
+<nav>
+    <div class="list-group">
+    <a type="button" class="btn btn-primary btn-lg" class="list-group-item list-group-item-action active">Home</a>
+    <a type="button" class="list-group-item list-group-item-action">Debogage</a>
+    <a type="button" class="list-group-item list-group-item-action">Concatenation</a>
+    <a type="button" class="list-group-item list-group-item-action">Boucles</a>
+    <a type="button" class="list-group-item list-group-item-action">Fonctions</a>
+    <a type="button" class="list-group-item list-group-item-action">Supprimer</a>
+</nav>
--- php/index.php
+++ php/index.php
@@ -1,3 +1,29 @@
-<?php
+// phpinfo();
 
-phpinfo();
+<?php include 'includes/head.inc.html'; ?>
+
+<?php include 'includes/header.inc.html'; ?>
+
+<div class="d-inline-flex p-1">
+
+<?php include 'includes/ul.inc.php' ?>
+
+<button href="?idPage=form" type="button" class="btn btn-primary btn-lg">  Ajouter des données</button>
+
+<?php
+if (isset($_GET["idPage"])) {
+    switch($_GET['idPage']) {
+        case 'form':
+            include "./includes/form.inc.html";
+            break;
+        default:
+            include("./includes/404.inc.html");
+    }
+    }else {
+        include "./includes/form2.inc.html";
+    }
+?>
+</div>
+
+
+<?php include 'includes/footer.inc.html'; ?>
