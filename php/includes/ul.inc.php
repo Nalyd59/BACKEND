a984595fbf897d3fd5ed419d0714f3c28e328ebf
Nalyd59 <dy.crepin@gmail.com>
2023-06-16 16:09:24 +0200

    finish

--- php/includes/ul.inc.php
+++ php/includes/ul.inc.php
@@ -1,7 +1,7 @@
 <nav>
     <div class="list-group">
 
-    <a type="button" class="btn btn-secondary btn-lg" class="list-group-item list-group-item-action active" href="?id=home" >Home</a>
+    <a type="button" class="btn btn-secondary btn-lg" class="list-group-item list-group-item-action active" href="/" >Home</a>
 
     <a id='hide' type="button" class="list-group-item list-group-item-action" href="?id=debug" >Debogage</a>
     <a id='hide' type="button" class="list-group-item list-group-item-action" href="?id=concat" >Concatenation</a>
